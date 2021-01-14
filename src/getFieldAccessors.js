36d2726095e41b7eded8e6ec6f1e0a5a18614b5b
jordan b <jordan.brull@fhegroupe.com>
2021-01-14 14:34:07 +0100

    Refac fieldAccessors

--- src/getFieldAccessors.js
+++ src/getFieldAccessors.js
@@ -1,7 +1,13 @@
-const curry = require('js-utils/curry');
+const { curry, isPlainObject } = require('js-utils');
 
-const _getFieldValue = (state, fieldName) => state[fieldName].value;
-const _getFieldError = (state, fieldName) => state[fieldName].error;
+const _getFieldValue = state => fieldName => {
+    if(!isPlainObject(state[fieldName])) {
+        return undefined;
+    }
+
+    return state[fieldName].value;
+};
+const _getFieldError = state => fieldName => state[fieldName].error;
 const _setFieldValue = ([state, setState]) => curry((fieldName, value) =>
     setState({
         [fieldName]: {
@@ -12,8 +18,8 @@ const _setFieldValue = ([state, setState]) => curry((fieldName, value) =>
 
 
 const getFieldAccessors = (state, setState) => ({
-    getFieldValue: _getFieldValue,
-    getFieldError: _getFieldError,
+    getFieldValue: _getFieldValue(state),
+    getFieldError: _getFieldError(state),
     setFieldValue: _setFieldValue([state, setState]),
 });
 
